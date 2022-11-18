95ffa743a2526120a6f49696b611e6b9357ed370
zEEmperon <dan4ik2204.danya@gmail.com>
2022-11-18 20:36:22 +0200

    added class calculations and improved correlation field

--- main.py
+++ main.py
@@ -21,11 +21,23 @@ def main():
     x_threshold_value = X.mean()
     y_threshold_value = Y.mean()
 
+    dec_K1_div_K1 = df[(df['U'] <= x_threshold_value) & (df['a'] <= y_threshold_value)]
+    dec_K1_div_K2 = df[(df['U'] <= x_threshold_value) & (df['a'] > y_threshold_value)]
+    dec_K2_div_K2 = df[(df['U'] > x_threshold_value) & (df['a'] > y_threshold_value)]
+    dec_K2_div_K1 = df[(df['U'] > x_threshold_value) & (df['a'] <= y_threshold_value)]
+
     # showing the correlation field
-    plt.plot(X, Y, 'go')
+    plt.plot(dec_K1_div_K1['U'], dec_K1_div_K1['a'], 'go', label='ріш К1 / К1')
+    plt.plot(dec_K1_div_K2['U'], dec_K1_div_K2['a'], 'ro', label='ріш К1 / К2')
+    plt.plot(dec_K2_div_K2['U'], dec_K2_div_K2['a'], 'bo', label='ріш К2 / К2')
+    plt.plot(dec_K2_div_K1['U'], dec_K2_div_K1['a'], 'yo', label='ріш К2 / К1')
+
     plt.plot(np.full(2, x_threshold_value), [0, y_max])
     plt.plot([0, x_max], np.full(2, y_threshold_value))
+
     plt.title("Correlation field")
+    plt.legend()
+
     plt.show()
 
 
